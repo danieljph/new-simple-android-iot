3fba9af4e6df0445fb8f9128f99f52ebf1dc00aa
Daniel J. P. Hutapea <daniel.joi.ph@gmail.com>
2020-04-15 14:07:14 +0700

    Initial commit.

---
+++ app/src/main/java/id/recharge/new_simple_android_iot/MainActivity.kt
@@ -0,0 +1,16 @@
+package id.recharge.new_simple_android_iot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * @author Daniel Joi Partogi Hutapea
+ */
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
